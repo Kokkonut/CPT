67ccc9ea1e4dc5ef4d39731170bc2fc4840c5fd1
Nicolas Fraenkel <nicfraenkel@gmail.com>
2023-03-28 13:28:37 +1030

    refactored server/index.js to reflect new folder structure

--- server/index.js
+++ server/index.js
@@ -1,30 +1,35 @@
+// Import required modules
 const path = require("path");
 const express = require("express");
 const compression = require("compression");
 const morgan = require("morgan");
 const { createRequestHandler } = require("@remix-run/express");
 
-const BUILD_DIR = path.join(process.cwd(), "build");
+// Set the path to the build directory (where the Remix application's static files are located)
+const BUILD_DIR = path.join(__dirname, "../build");
 
+// Create an instance of the Express application
 const app = express();
 
+// Use the compression middleware to enable gzip compression of HTTP responses
 app.use(compression());
 
-// http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
+// Disable the "x-powered-by" header for security reasons
 app.disable("x-powered-by");
 
-// Remix fingerprints its assets so we can cache forever.
+// Serve static assets from the build directory, with a cache expiration of 1 year
 app.use(
   "/build",
   express.static("public/build", { immutable: true, maxAge: "1y" })
 );
 
-// Everything else (like favicon.ico) is cached for an hour. You may want to be
-// more aggressive with this caching.
+// Serve other static assets (such as favicon.ico) from the public directory, with a cache expiration of 1 hour
 app.use(express.static("public", { maxAge: "1h" }));
 
+// Use the Morgan middleware to log HTTP requests in a "tiny" format to the console
 app.use(morgan("tiny"));
 
+// Handle all HTTP requests with the Remix application's request handler
 app.all(
   "*",
   process.env.NODE_ENV === "development"
@@ -41,18 +46,19 @@ app.all(
         mode: process.env.NODE_ENV,
       })
 );
+
+// Start the Express application on the specified port
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
 });
 
+// A helper function to purge the Node.js require cache for the build directory
 function purgeRequireCache() {
-  // purge require cache on requests for "server side HMR" this won't let
-  // you have in-memory objects between requests in development,
-  // alternatively you can set up nodemon/pm2-dev to restart the server on
-  // file changes, but then you'll have to reconnect to databases/etc on each
-  // change. We prefer the DX of this, so we've included it for you by default
+  // This purges the require cache on requests for "server side HMR" (Hot Module Replacement).
+  // This prevents in-memory objects from persisting between requests in development mode.
+  // In production mode, the require cache is not purged between requests.
   for (const key in require.cache) {
     if (key.startsWith(BUILD_DIR)) {
       delete require.cache[key];
