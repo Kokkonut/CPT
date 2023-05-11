0e0c49115ebf12a3ceb967dead229ff544d0368f
Nicolas Fraenkel <nicfraenkel@gmail.com>
2023-05-11 16:40:01 +0930

    added join org to new dashboard'

--- app/routes/dashboard/join-org/index.tsx
+++ app/routes/dashboard/join-org/index.tsx
@@ -1,11 +1,10 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form } from '@remix-run/react';
+import DashboardLayout from '~/layouts/Dashboardlayout';
+import Breadcrumb from '~/components/Breadcrumb';
 
-interface JoinOrgModalProps {
-  closeModal: () => void;
-}
 
-const JoinOrg: React.FC<JoinOrgModalProps> = ({ closeModal }) => {
+function JoinOrg() {
   const [showForm, setShowForm] = useState(true);
 
   useEffect(() => {
@@ -25,7 +24,6 @@ const JoinOrg: React.FC<JoinOrgModalProps> = ({ closeModal }) => {
     if (response.ok) {
       // Redirect or show a success message
       console.log('Successfully joined organization');
-      closeModal();
     } else {
       // Show an error message
       console.error('Failed to join organization');
@@ -33,45 +31,36 @@ const JoinOrg: React.FC<JoinOrgModalProps> = ({ closeModal }) => {
   }
 
   return (
-    <Fragment>
-      <div className="fixed inset-0 bg-gray-500 bg-opacity-50 z-50"></div>
-      <div className="fixed inset-0 flex justify-center items-center z-50">
-        <div className="bg-white rounded-lg p-4 w-1/2">
-          <h2 className="text-2xl font-bold mb-4">Join Organization</h2>
-          {showForm && (
-            <Form method="post" onSubmit={handleSubmit} className="space-y-4">
-              <div className="flex flex-col">
-                <label htmlFor="orgId" className="font-medium">
-                  Organization Name:
-                </label>
-                <input
-                  id="orgName"
-                  name="orgName"
-                  type="text"
-                  required
-                  className="border border-gray-300 p-2 rounded"
-                />
-              </div>
-              <div className="flex justify-end">
-                <button
-                  type="button"
-                  onClick={closeModal}
-                  className="mr-2 bg-gray-400 text-white font-semibold py-2 px-4 rounded hover:bg-gray-600"
-                >
-                  Cancel
-                </button>
-                <button
-                  type="submit"
-                  className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700"
-                >
-                  Join Organization
-                </button>
-              </div>
-            </Form>
-          )}
-        </div>
-      </div>
-    </Fragment>
+    <DashboardLayout>
+    <Breadcrumb pageName="Join Organization" />
+    <div className="bg-white rounded-lg p-4 w-1/2">
+      <h2 className="text-2xl font-bold mb-4">Join Organization</h2>
+      {showForm && (
+        <Form method="post" onSubmit={handleSubmit} className="space-y-4">
+          <div className="flex flex-col">
+            <label htmlFor="orgId" className="font-medium">
+              Organization Name:
+            </label>
+            <input
+              id="orgName"
+              name="orgName"
+              type="text"
+              required
+              className="border border-gray-300 p-2 rounded"
+            />
+          </div>
+          <div className="flex justify-end">
+            <button
+              type="submit"
+              className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-700"
+            >
+              Join Organization
+            </button>
+          </div>
+        </Form>
+      )}
+    </div>
+    </DashboardLayout>
   );
 };
 
