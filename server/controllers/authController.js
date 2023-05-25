3456c01779c6d81371dd9c2f6c7e1bdc9e5dcedd
Nicolas Fraenkel <nicfraenkel@gmail.com>
2023-05-25 16:51:23 +0930

    Fixed bug with cookie

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -42,8 +42,7 @@ exports.loginUser = async (req, res) => {
   }
 };
 
-
-//signup  
+//signup
 exports.signup = async (req, res) => {
   try {
     const { firstName, lastName, email, password } = req.body;
@@ -52,11 +51,20 @@ exports.signup = async (req, res) => {
     const user = new User({ firstName, lastName, email, password });
     await user.save();
 
-    // Create a JWT for the user
-    const token = jwt.sign({ userId: user._id }, 'secretKey', { expiresIn: '1h' }); // Use your secret key here and set the expiration time as per your requirements
-
-    // Set the JWT in an HttpOnly cookie
-    res.cookie('token', token, { httpOnly: true, maxAge: 3600000 }); // 1 hour cookie
+   //added manager flag to token
+   const token = jwt.sign(
+    { id: user.id, role: user.manager ? "manager" : "user" },
+    jwtSecret,
+    { expiresIn: "1d" }
+  );
+  //max age 24 hours for cookie
+  const isProduction = process.env.NODE_ENV === "production";
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: isProduction,
+    maxAge: 24 * 60 * 60 * 1000,
+    sameSite: isProduction ? "none" : undefined,
+  });
 
     res.status(200).json({ message: "User created", token: token });
   } catch (err) {
@@ -64,14 +72,13 @@ exports.signup = async (req, res) => {
   }
 };
 
-
 //signup with invite
 exports.signupWithInvite = async (req, res) => {
   try {
     const { firstName, lastName, email, password, inviteId } = req.body;
 
     // Look for the corresponding invite
-    const invite = await Invite.findById(inviteId).populate('organization');
+    const invite = await Invite.findById(inviteId).populate("organization");
     if (!invite) {
       return res.status(400).json({ message: "Invalid invite" });
     }
@@ -92,13 +99,21 @@ exports.signupWithInvite = async (req, res) => {
     // Remove the used invite
     await invite.deleteOne();
 
-    // Create a JWT for the user
-    const token = jwt.sign({ userId: user._id }, 'secretKey', { expiresIn: '1h' }); // Use your secret key here and set the expiration time as per your requirements
+   //added manager flag to token
+   const token = jwt.sign(
+    { id: user.id, role: user.manager ? "manager" : "user" },
+    jwtSecret,
+    { expiresIn: "1d" }
+  );
+  //max age 24 hours for cookie
+  const isProduction = process.env.NODE_ENV === "production";
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: isProduction,
+    maxAge: 24 * 60 * 60 * 1000,
+    sameSite: isProduction ? "none" : undefined,
+  });
 
-    // Set the JWT in an HttpOnly cookie
-    res.cookie('token', token, { httpOnly: true, maxAge: 3600000 }); // 1 hour cookie
-
-    res.status(200).json({ message: "User created and added to organization", token: token });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: err.message });
